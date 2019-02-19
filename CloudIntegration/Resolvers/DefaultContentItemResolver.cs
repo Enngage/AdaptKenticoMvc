7ee5fa22e778403f687ab1fbc34d9af9033bffc7
Enngage <ennspassion@gmail.com>
2019-02-19 13:40:30 +0100

    Update to .NET Core 2.2 and delivery-sdk-net 9.x with appropriate code changes

--- CloudIntegration/Resolvers/DefaultContentItemResolver.cs
+++ CloudIntegration/Resolvers/DefaultContentItemResolver.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using CloudIntegration.Models.Cloud;
 using KenticoCloud.Delivery.InlineContentItems;
 
@@ -10,20 +9,22 @@ namespace CloudIntegration.Resolvers
 
         public string Resolve(ResolvedContentItemData<object> data)
         {
-            #warning This is a temporary fix due to bug where inlinte content resolvers do not work in Delivery sdk v8
-
-            if (data.Item is InfoBox infoBox)
+            if (data.Item is ResolvedContentItemData<object> itemWrapper)
             {
-                return $"<div>{infoBox.Content}</div>";
+                if (itemWrapper.Item is InfoBox infoBox)
+                {
+                    return $"<div>{infoBox.Content}</div>";
+                }
+
+                if (itemWrapper.Item is CodeBlock codeBlock)
+                {
+                    return
+                        $"<pre><code class=\"language-{codeBlock.AvailableLanguagesLanguage?.FirstOrDefault()?.Codename.ToLower().Trim()}\">" +
+                            $"{System.Web.HttpUtility.HtmlEncode(codeBlock.Code)?.Trim()}" +
+                        $"</code></pre>";
+                }
             }
 
-            if (data.Item is CodeBlock codeBlock)
-            {
-                return
-                    $"<pre><code class=\"language-{codeBlock.AvailableLanguagesLanguage?.FirstOrDefault()?.Codename.ToLower().Trim()}\">" +
-                        $"{System.Web.HttpUtility.HtmlEncode(codeBlock.Code)?.Trim()}" +
-                    $"</code></pre>";
-            }
             return "Content not available.";
         }
     }
